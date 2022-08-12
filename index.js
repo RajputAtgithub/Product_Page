1a70e42bba74547df0a116301740f5290167428f
Suraj Rajput <34212964+RajputAtgithub@users.noreply.github.com>
2022-08-12 21:20:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,616 @@
+var data = [
+    {
+      id: 1,
+      imageURL:
+        "https://geektrust.s3.ap-southeast-1.amazonaws.com/coding-problems/shopping-cart/polo-tshirts.png",
+      name: "Black Polo",
+      type: "Polo",
+      price: 250,
+      currency: "INR",
+      color: "Black",
+      gender: "Men",
+      quantity: 3
+    },
+    {
+      id: 2,
+      imageURL:
+        "https://geektrust.s3.ap-southeast-1.amazonaws.com/coding-problems/shopping-cart/polo-tshirts.png",
+      name: "Blue Polo",
+      type: "Polo",
+      price: 350,
+      currency: "INR",
+      color: "Blue",
+      gender: "Women",
+      quantity: 3
+    },
+    {
+      id: 3,
+      imageURL:
+        "https://geektrust.s3.ap-southeast-1.amazonaws.com/coding-problems/shopping-cart/polo-tshirts.png",
+      name: "Pink Polo",
+      type: "Polo",
+      price: 350,
+      currency: "INR",
+      color: "Pink",
+      gender: "Women",
+      quantity: 6
+    },
+    {
+      id: 4,
+      imageURL:
+        "https://geektrust.s3.ap-southeast-1.amazonaws.com/coding-problems/shopping-cart/hoodie-tshirts.png",
+      name: "Black Hoodie",
+      type: "Hoodie",
+      price: 500,
+      currency: "INR",
+      color: "Black",
+      gender: "Men",
+      quantity: 2
+    },
+    {
+      id: 5,
+      imageURL:
+        "https://geektrust.s3.ap-southeast-1.amazonaws.com/coding-problems/shopping-cart/polo-tshirts.png",
+      name: "Green Polo",
+      type: "Polo",
+      price: 250,
+      currency: "INR",
+      color: "Green",
+      gender: "Men",
+      quantity: 1
+    },
+    {
+      id: 6,
+      imageURL:
+        "https://geektrust.s3.ap-southeast-1.amazonaws.com/coding-problems/shopping-cart/polo-tshirts.png",
+      name: "Green Polo",
+      type: "Polo",
+      price: 350,
+      currency: "INR",
+      color: "Green",
+      gender: "Women",
+      quantity: 1
+    },
+    {
+      id: 7,
+      imageURL:
+        "https://geektrust.s3.ap-southeast-1.amazonaws.com/coding-problems/shopping-cart/hoodie-tshirts.png",
+      name: "Blue Hoodie",
+      type: "Hoodie",
+      price: 500,
+      currency: "INR",
+      color: "Blue",
+      gender: "Women",
+      quantity: 2
+    },
+    {
+      id: 8,
+      imageURL:
+        "https://geektrust.s3.ap-southeast-1.amazonaws.com/coding-problems/shopping-cart/hoodie-tshirts.png",
+      name: "Black Hoodie",
+      type: "Hoodie",
+      price: 500,
+      currency: "INR",
+      color: "Black",
+      gender: "Women",
+      quantity: 5
+    },
+    {
+      id: 9,
+      imageURL:
+        "https://geektrust.s3.ap-southeast-1.amazonaws.com/coding-problems/shopping-cart/round-neck-tshirts.png",
+      name: "Blue Round",
+      type: "Basic",
+      price: 300,
+      currency: "INR",
+      color: "Blue",
+      gender: "Men",
+      quantity: 2
+    },
+    {
+      id: 10,
+      imageURL:
+        "https://geektrust.s3.ap-southeast-1.amazonaws.com/coding-problems/shopping-cart/round-neck-tshirts.png",
+      name: "Red Round",
+      type: "Basic",
+      price: 300,
+      currency: "INR",
+      color: "Red",
+      gender: "Women",
+      quantity: 2
+    },
+    {
+      id: 11,
+      imageURL:
+        "https://geektrust.s3.ap-southeast-1.amazonaws.com/coding-problems/shopping-cart/round-neck-tshirts.png",
+      name: "Grey Round",
+      type: "Basic",
+      price: 300,
+      currency: "INR",
+      color: "Grey",
+      gender: "Men",
+      quantity: 1
+    },
+    {
+      id: 12,
+      imageURL:
+        "https://geektrust.s3.ap-southeast-1.amazonaws.com/coding-problems/shopping-cart/hoodie-tshirts.png",
+      name: "Purple Hoodie",
+      type: "Hoodie",
+      price: 500,
+      currency: "INR",
+      color: "Purple",
+      gender: "Women",
+      quantity: 3
+    },
+    {
+      id: 13,
+      imageURL:
+        "https://geektrust.s3.ap-southeast-1.amazonaws.com/coding-problems/shopping-cart/round-neck-tshirts.png",
+      name: "Grey Round",
+      type: "Basic",
+      price: 300,
+      currency: "INR",
+      color: "Grey",
+      gender: "Women",
+      quantity: 1
+    },
+    {
+      id: 14,
+      imageURL:
+        "https://geektrust.s3.ap-southeast-1.amazonaws.com/coding-problems/shopping-cart/round-neck-tshirts.png",
+      name: "White Round",
+      type: "Basic",
+      price: 300,
+      currency: "INR",
+      color: "White",
+      gender: "Women",
+      quantity: 0
+    },
+    {
+      id: 15,
+      imageURL:
+        "https://geektrust.s3.ap-southeast-1.amazonaws.com/coding-problems/shopping-cart/round-neck-tshirts.png",
+      name: "Black Round",
+      type: "Basic",
+      price: 300,
+      currency: "INR",
+      color: "Black",
+      gender: "Men",
+      quantity: 7
+    },
+    {
+      id: 16,
+      imageURL:
+        "https://geektrust.s3.ap-southeast-1.amazonaws.com/coding-problems/shopping-cart/round-neck-tshirts.png",
+      name: "Purple Round",
+      type: "Basic",
+      price: 300,
+      currency: "INR",
+      color: "Purple",
+      gender: "Men",
+      quantity: 1
+    },
+    {
+      id: 17,
+      imageURL:
+        "https://geektrust.s3.ap-southeast-1.amazonaws.com/coding-problems/shopping-cart/round-neck-tshirts.png",
+      name: "White Round",
+      type: "Basic",
+      price: 300,
+      currency: "INR",
+      color: "White",
+      gender: "Men",
+      quantity: 2
+    },
+    {
+      id: 18,
+      imageURL:
+        "https://geektrust.s3.ap-southeast-1.amazonaws.com/coding-problems/shopping-cart/round-neck-tshirts.png",
+      name: "Blue Round",
+      type: "Basic",
+      price: 300,
+      currency: "INR",
+      color: "Blue",
+      gender: "Women",
+      quantity: 3
+    },
+    {
+      id: 19,
+      imageURL:
+        "https://geektrust.s3.ap-southeast-1.amazonaws.com/coding-problems/shopping-cart/hoodie-tshirts.png",
+      name: "Yellow Hoodie",
+      type: "Hoodie",
+      price: 500,
+      currency: "INR",
+      color: "Yellow",
+      gender: "Women",
+      quantity: 1
+    },
+    {
+      id: 20,
+      imageURL:
+        "https://geektrust.s3.ap-southeast-1.amazonaws.com/coding-problems/shopping-cart/polo-tshirts.png",
+      name: "White Polo",
+      type: "Polo",
+      price: 350,
+      currency: "INR",
+      color: "White",
+      gender: "Women",
+      quantity: 4
+    },
+    {
+      id: 21,
+      imageURL:
+        "https://geektrust.s3.ap-southeast-1.amazonaws.com/coding-problems/shopping-cart/polo-tshirts.png",
+      name: "Red Polo",
+      type: "Polo",
+      price: 250,
+      currency: "INR",
+      color: "Red",
+      gender: "Men",
+      quantity: 2
+    },
+    {
+      id: 22,
+      imageURL:
+        "https://geektrust.s3.ap-southeast-1.amazonaws.com/coding-problems/shopping-cart/hoodie-tshirts.png",
+      name: "Blue Hoodie",
+      type: "Hoodie",
+      price: 500,
+      currency: "INR",
+      color: "Blue",
+      gender: "Men",
+      quantity: 0
+    },
+    {
+      id: 23,
+      imageURL:
+        "https://geektrust.s3.ap-southeast-1.amazonaws.com/coding-problems/shopping-cart/hoodie-tshirts.png",
+      name: "Grey Hoodie",
+      type: "Hoodie",
+      price: 500,
+      currency: "INR",
+      color: "Grey",
+      gender: "Men",
+      quantity: 2
+    },
+    {
+      id: 24,
+      imageURL:
+        "https://geektrust.s3.ap-southeast-1.amazonaws.com/coding-problems/shopping-cart/polo-tshirts.png",
+      name: "Grey Polo",
+      type: "Polo",
+      price: 300,
+      currency: "INR",
+      color: "Grey",
+      gender: "Men",
+      quantity: 3
+    },
+    {
+      id: 25,
+      imageURL:
+        "https://geektrust.s3.ap-southeast-1.amazonaws.com/coding-problems/shopping-cart/hoodie-tshirts.png",
+      name: "Red Hoodie",
+      type: "Hoodie",
+      price: 300,
+      currency: "INR",
+      color: "Red",
+      gender: "Men",
+      quantity: 0
+    },
+    {
+      id: 26,
+      imageURL:
+        "https://geektrust.s3.ap-southeast-1.amazonaws.com/coding-problems/shopping-cart/polo-tshirts.png",
+      name: "White Polo",
+      type: "Polo",
+      price: 300,
+      currency: "INR",
+      color: "White",
+      gender: "Men",
+      quantity: 1
+    },
+    {
+      id: 27,
+      imageURL:
+        "https://geektrust.s3.ap-southeast-1.amazonaws.com/coding-problems/shopping-cart/hoodie-tshirts.png",
+      name: "White Hoodie",
+      type: "Hoodie",
+      price: 500,
+      currency: "INR",
+      color: "White",
+      gender: "Women",
+      quantity: 3
+    },
+    {
+      id: 28,
+      imageURL:
+        "https://geektrust.s3.ap-southeast-1.amazonaws.com/coding-problems/shopping-cart/round-neck-tshirts.png",
+      name: "Grey Round",
+      type: "Basic",
+      price: 300,
+      currency: "INR",
+      color: "Grey",
+      gender: "Men",
+      quantity: 0
+    },
+    {
+      id: 29,
+      imageURL:
+        "https://geektrust.s3.ap-southeast-1.amazonaws.com/coding-problems/shopping-cart/round-neck-tshirts.png",
+      name: "Black Round",
+      type: "Basic",
+      price: 300,
+      currency: "INR",
+      color: "Black",
+      gender: "Women",
+      quantity: 0
+    },
+    {
+      id: 30,
+      imageURL:
+        "https://geektrust.s3.ap-southeast-1.amazonaws.com/coding-problems/shopping-cart/polo-tshirts.png",
+      name: "Black Polo",
+      type: "Polo",
+      price: 300,
+      currency: "INR",
+      color: "Black",
+      gender: "Women",
+      quantity: 4
+    }
+  ];
+  
+  
+/* function change() {
+    var colourCbs=document.querySelectorAll('.filter.colours input[type="checkbox"]');
+    var genderCbs=document.querySelectorAll('.filter.genders input[type="checkbox"]');
+    var priceCbs=document.querySelectorAll('.filter.price input[type="checkbox"]');
+    var typesCbs=document.querySelectorAll('.filter.types input[type="checkbox"]');
+
+    var filters= {colours:getClassOfCheckedCheckboxes(colourCbs),
+        genders:getClassOfCheckedCheckboxes(genderCbs),
+        prices:getClassOfCheckedCheckboxes(priceCbs),
+        types:getClassOfCheckedCheckboxes(typesCbs) 
+    };
+    filterResults(filters);
+}
+
+function getClassOfCheckedCheckboxes(checkboxes){
+    var classes=[];
+    if(checkboxes && checkboxes.length > 0){
+        for (i=0; i<checkboxes.length; i++){
+            var cb=checkboxes[i];
+            if(cb.checked){
+                classes.push(cb.getAttribute("rel"));
+            }
+        }
+    }
+    return classes;
+}
+function filterResults(filters){
+    var resultElement = document.querySelectorAll(".result > div");
+    var hiddenElement=[];
+
+    if(!resultElement || resultElement.length<=0){
+
+            return;
+
+     }       
+    for (var i=0; i<resultElement.length; i++) {
+        var element=resultElement[i];
+
+        if(filters.colours.length > 0){
+            var isHidden=true;
+                for (var j=0; j<filters.colours.length; j++) {
+                
+                    var filter=filters.colours[j];
+console.log(element.classList.contains(filter));
+                    if(element.classList.contains(filter)){
+                        isHidden=false;
+                        break;
+                    }
+                }
+                    if(isHidden){
+                        hiddenElement.push(element);
+                    }
+        }
+    
+
+        if(filters.genders.length > 0){
+            var isHidden=true;
+                for (var j=0; j<filters.genders.length; j++) {
+                
+                    var filter=filters.genders[j];
+
+                    if(element.classList.contains(filter)){
+                        isHidden=false;
+                        break;
+                    }
+                }
+                    if(isHidden){
+                        hiddenElement.push(element);
+                    }
+        }
+        
+    
+
+
+        if(filters.prices.length > 0){
+            var isHidden=true;
+                for (var j=0; j<filters.prices.length; j++) {
+                
+                    var filter=filters.prices[j];
+
+                    if(element.classList.contains(filter)){
+                        isHidden=false;
+                        break;
+                    }
+                }
+                    if(isHidden){
+                        hiddenElement.push(element);
+                    }
+        }
+        
+
+        
+    
+        if(filters.types.length > 0){
+            var isHidden=true;
+                for (var j=0; j<filters.types.length; j++) {
+                
+                    var filter=filters.types[j];
+
+                    if(element.classList.contains(filter)){
+                        isHidden=false;
+                        break;
+                    }
+                }
+                    if(isHidden){
+                        hiddenElement.push(element);
+                    }
+        }
+    
+
+        
+
+        
+    
+}
+        
+    
+    for (var i = 0; i < resultElement.length; i++) {
+        resultElement[i].style.display = "block";
+      }
+    
+      if (hiddenElement.length <= 0) {
+        return;
+      }
+    
+      for (var i = 0; i < hiddenElement.length; i++) {
+        hiddenElement[i].style.display = "none";
+      }
+} */
+function change() {
+  var colourCb = document.querySelectorAll(".colours input[type='checkbox']");
+  var genderCb = document.querySelectorAll(".genders input[type='checkbox']");
+  var priceCb = document.querySelectorAll(".price input[type='checkbox']");
+  var typeCb = document.querySelectorAll(".types input[type='checkbox']");
+  var filters = {
+    colours: getClassOfCheckedCheckboxes(colourCb),
+    genders: getClassOfCheckedCheckboxes(genderCb),
+    prices: getClassOfCheckedCheckboxes(priceCb),
+    types: getClassOfCheckedCheckboxes(typeCb)
+  };
+
+  filterResults(filters);
+}
+
+function getClassOfCheckedCheckboxes(checkboxes) {
+  var classes = [];
+
+  if (checkboxes && checkboxes.length > 0) {
+    for (var i = 0; i < checkboxes.length; i++) {
+      var cb = checkboxes[i];
+
+      if (cb.checked) {
+        classes.push(cb.getAttribute("rel"));
+      }
+    }
+  }
+
+  return classes;
+}
+
+function filterResults(filters) {
+  var rElems = document.querySelectorAll(".result div");
+  var hiddenElems = [];
+
+  if (!rElems || rElems.length <= 0) {
+    return;
+  }
+
+  for (var i = 0; i < rElems.length; i++) {
+    var el = rElems[i];
+
+    if (filters.colours.length > 0) {
+      var isHidden = true;
+
+      for (var j = 0; j < filters.colours.length; j++) {
+        var filter = filters.colours[j];
+
+        if (el.classList.contains(filter)) {
+          isHidden = false;
+          break;
+        }
+      }
+
+      if (isHidden) {
+        hiddenElems.push(el);
+      }
+    }
+
+    if (filters.genders.length > 0) {
+      var isHidden = true;
+
+      for (var j = 0; j < filters.genders.length; j++) {
+        var filter = filters.genders[j];
+
+        if (el.classList.contains(filter)) {
+          isHidden = false;
+          break;
+        }
+      }
+
+      if (isHidden) {
+        hiddenElems.push(el);
+      }
+    }
+
+    if (filters.prices.length > 0) {
+      var isHidden = true;
+
+      for (var j = 0; j < filters.prices.length; j++) {
+        var filter = filters.prices[j];
+
+        if (el.classList.contains(filter)) {
+          isHidden = false;
+          break;
+        }
+      }
+
+      if (isHidden) {
+        hiddenElems.push(el);
+      }
+    }
+
+    if (filters.types.length > 0) {
+      var isHidden = true;
+
+      for (var j = 0; j < filters.types.length; j++) {
+        var filter = filters.types[j];
+
+        if (el.classList.contains(filter)) {
+          isHidden = false;
+          break;
+        }
+      }
+
+      if (isHidden) {
+        hiddenElems.push(el);
+      }
+    }
+  }///////////////-------------i for loop ends here--------/////////////////
+
+  for (var i = 0; i < rElems.length; i++) {
+    rElems[i].style.display = "block";
+  }
+
+  if (hiddenElems.length <= 0) {
+    return;
+  }
+
+  for (var i = 0; i < hiddenElems.length; i++) {
+    hiddenElems[i].style.display = "none";
+  }
+}
